fd1dc11dc509e49798fe2fa727adfccaea8fdb8d
becall10 <becall10@gmail.com>
2023-12-05 23:39:34 -0500

    updated pages

--- internal-app/src/App.js
+++ internal-app/src/App.js
@@ -3,10 +3,17 @@ import React, { useState, useEffect } from "react";
 import ApplicationCard from "./ApplicationCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './NavigationBar';
+import Home from './Home';
+import Application from './Application';
+import Contact from './Contact';
+
 
 const API_URL = "http://www.omdbapi.com?apikey=b88a1e25";
 
 const App = () => {
+
   const [searchTerm, setSearchTerm] = useState("");
   const [applications, setapplications] = useState([]);
 
@@ -24,6 +31,16 @@ const App = () => {
   return (
     <div className="app">
       <h1>CFO PORTAL</h1>
+      <Router>
+      <div className='navBar'>
+        <NavigationBar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/apps" element={<Application />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
 
       <div className="search">
         <input
---
+++ internal-app/src/Application.jsx
@@ -0,0 +1,9 @@
+
+const Application = () => {
+    return (
+      <div>
+        <h2>Apps</h2>
+      </div>
+    );
+  };
+  export default Application;
